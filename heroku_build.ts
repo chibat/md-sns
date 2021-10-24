27b536188ceb4bb47f35d5695d4a3a5554d614a6
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-10-24 20:58:42 +0900

    fix

--- heroku_build.ts
+++ heroku_build.ts
@@ -1,66 +1,8 @@
 #!/usr/bin/env -S deno run -A --unstable
 
-import { deno, denoDir } from "./tasks_utils.ts";
+import { $ } from "./tasks_utils.ts";
 
-const COMMAND_PATH="https://deno.land/x/aleph@v0.3.0-beta.19/commands";
+await $(["bash", "-c", "deno cache --import-map=import_map.json --no-check --unstable *.ts *.ts */*.ts */*.tsx */*/*.tsx */*/*/*.tsx"]);
 
-const home = Deno.env.get("HOME");
-if (!home) {
-  console.error("env.HOME");
-  Deno.exit(1);
-}
-
-const esbuild = `${home}/.cache/esbuild/bin/esbuild-linux-64@0.13.2`;
-
-/*
-await deno({
-  command: "run",
-  "--allow-net": [
-    "deno.land",
-    "esm.sh",
-    "cdn.esm.sh",
-    "registry.npmjs.org",
-    "cdnjs.cloudflare.com",
-    "cdn.jsdelivr.net",
-  ],
-  "--allow-read": true,
-  "--allow-env": [
-    "ALEPH_DEV",
-    "DENO_TESTING",
-    "ALEPH_DEV_PORT",
-    "ALEPH_ENV",
-    "ALEPH_FRAMEWORK",
-    "ALEPH_WORKING_DIR",
-    "ALEPH_VERSION",
-    "ESBUILD_BINARY_PATH",
-    "XDG_CACHE_HOME",
-    "HOME",
-  ],
-  "--allow-write": [
-    "./.aleph",
-    "./dist",
-    await denoDir(),
-    `${home}/.cache`,
-  ],
-  "--allow-run": [
-    Deno.execPath(),
-    esbuild,
-  ],
-  args: [
-    "--import-map=import_map.json",
-    "--unstable",
-    `${COMMAND_PATH}/build.ts`,
-  ],
-});
-*/
-
-await deno({
-  command: "cache",
-  args: [
-    "--import-map=import_map.json",
-    "--unstable",
-    "lib/db.ts",
-  ],
-});
 
 
